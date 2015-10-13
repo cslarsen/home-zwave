607e3b536a806b816474a70aaf203b2f7a893285
Christian Stigen Larsen <csl@csl.name>
2015-10-13 21:01:13 +0200

    Adds sqlite3 backend

--- home.py
+++ home.py
@@ -3,22 +3,110 @@
 
 from openzwave.network import ZWaveNetwork
 from openzwave.option import ZWaveOption
+import Queue
 import code
+import collections
+import contextlib
 import datetime
 import logging
 import louie
 import os
+import sqlite3
 import sys
+import threading
 import time
+
+class Db:
+  def __init__(self, location):
+    self.location = location
+    self.con = sqlite3.connect(location)
+    if self.empty():
+      self.create_schema()
+
+  @contextlib.contextmanager
+  def cursor(self):
+    c = self.con.cursor()
+    yield c
+    c.close()
+
+  def close(self):
+    """Flushes changes to disk and closes connection."""
+    self.con.commit()
+    self.con.close()
+
+  def empty(self):
+    with self.cursor() as c:
+      return len(c.execute("select * from SQLITE_MASTER").fetchall()) == 0
+
+  def create_schema(self):
+    logging.info("Creating database %s" % self.location)
+
+    with self.cursor() as c:
+      c.execute("""create table value (
+                    recorded_utc datetime default current_timestamp,
+                    value_id integer,
+                    value real)""")
+
+  def add_value(self, timestamp, value):
+    with self.cursor() as c:
+      c.execute("insert into value (recorded_utc, value_id, value) values (?,?,?)",
+          (timestamp, value.value_id, float(value.data)))
+
+class DataQueue:
+  queue = Queue.Queue(10000)
+  running = True
+
+  def __init__(self, location):
+    self.db = Db(location)
+
+  @staticmethod
+  def worker(*args, **kw):
+    logging.info("Database location: %s" % kw.get("location", "<unset>"))
+    queue = DataQueue(kw.get("location", ":memory:"))
+
+    # Time until we save to disk
+    commit_timeout = datetime.timedelta(seconds=30)
+
+    # Time to wait for each queue item
+    get_timeout = 3
+
+    try:
+      start = datetime.datetime.utcnow()
+      while True:
+        try:
+          item = DataQueue.queue.get(block=True, timeout=get_timeout)
+          queue.db.add_value(*item)
+          DataQueue.queue.task_done()
+        except Queue.Empty:
+          if not DataQueue.running:
+            # Exit after saving remaining items
+            break
+          else:
+            # Flush database to disk after some time
+            now = datetime.datetime.utcnow()
+            if (now - start) >= commit_timeout:
+              start = now
+              queue.db.con.commit()
+    finally:
+      queue.db.close()
+
+  @staticmethod
+  def put(*args, **kw):
+    DataQueue.queue.put(*args, **kw)
+
+  @staticmethod
+  def stop():
+    DataQueue.running = False
+
 
 class Signal:
   @staticmethod
-  def node_updated(network, node):
-    logging.info("Node updated %s" % node)
+  def node_updated(*args, **kw):
+    logging.info("Node updated args=%s kw=%s" % (args, kw))
 
   @staticmethod
   def node_event(*args, **kw):
-    logging.info("node event args=%s kw=%s" % (args, kw))
+    logging.info("Node event args=%s kw=%s" % (args, kw))
 
   @staticmethod
   def value_updated(network, node, value):
@@ -26,13 +114,16 @@ class Signal:
     if not name:
       name = node.product_type
 
-    logging.info("s node_id=%s value_id=%x '%s' %s: %s %s" % (
+    logging.info("Value updated node_id=%s value_id=%x '%s' %s: %s %s" % (
       node.node_id,
       value.value_id,
       name,
       value.label,
       value.data,
       value.units))
+
+    if isinstance(value.data, float) or isinstance(value.data, int):
+      DataQueue.put((datetime.datetime.utcnow(), value), block=True)
 
   @staticmethod
   def network_started(network):
@@ -90,18 +181,24 @@ def main(device = "/dev/ttyACM0"): # TODO: Auto-discover
   louie.dispatcher.connect(Signal.network_ready, ZWaveNetwork.SIGNAL_NETWORK_READY)
   louie.dispatcher.connect(Signal.node_event, ZWaveNetwork.SIGNAL_NODE_EVENT)
 
-  # Find light switch
+  # TODO: Find light switch
+
+  queue = threading.Thread(target=DataQueue.worker,
+      kwargs={"location": "home.db"})
+  queue.start()
 
   try:
-    network.start()
-    #while True:
-    #  time.sleep(5)
-    code.interact(local=locals())
+      network.start()
+      code.interact(local=locals())
   except KeyboardInterrupt:
     pass
   finally:
     logging.info("\nStopping network ...")
     network.stop()
+
+    logging.info("Stopping data queue ...")
+    DataQueue.stop()
+    queue.join()
 
 if __name__ == "__main__":
   main()
